23eb58a526da7ac3d2fbb98d46ad8b679e7c3766
James Robinson <jpwrobinson@gmail.com>
2022-06-15 17:08:45 +0100

    export %

--- scripts/fig/FigureSX_export.R
+++ scripts/fig/FigureSX_export.R
@@ -3,7 +3,7 @@ theme_set(theme_sleek())
 
 source('scripts/fig/00_plotting.R')
 
-
+## nut ghg for axis order
 drops<-c('Other marine fish')
 nut<-read.csv('data/UK_GHG_nutrient_catch.csv') %>% 
   mutate(prop_tot = tot / all * 100, class = ifelse(prop_tot >= 5, 'High production', 'Low production')) %>% 
@@ -17,6 +17,22 @@ nut<-read.csv('data/UK_GHG_nutrient_catch.csv') %>%
   summarise_at(vars(low:nut_score2, vitamin_d:iodine_rda), mean) %>% 
   mutate(species=factor(species), id = paste0(species, ' (', farmed_wild, ')'))
 
+## available seafood
+nutS<-read.csv('data/UK_GHG_nutrient_catch_bysector.csv') %>% 
+  filter(species %in% nut$species) %>%
+  select(-tax) %>% 
+  group_by(species, farmed_wild, tot, catch, source) %>% 
+  mutate(species=factor(species), id = paste0(species, ' (', farmed_wild, ')')) %>% 
+  mutate(source = case_when(
+      str_detect(source, 'farmed') ~ 'Farmed (UK)',
+        str_detect(source, 'wild') ~ 'Wild (UK)',
+        str_detect(source, 'imported') ~ 'Imported'))
+
+nutS$source<-factor(nutS$source, levels=unique(nutS$source)[c(2,1,3)])
+nutS$class<-nut$class[match(nutS$species, nut$species)]
+nutS<-nutS %>% group_by(species, class, source, tot, mid) %>% summarise(catch = sum(catch)) %>% 
+		group_by(species) %>% 
+		summarise(tot = unique(tot))
 
 ## read export data
 ex<-readxl::read_excel('data/uk/uk_export.xlsx') %>% clean_names() %>% 
@@ -26,6 +42,8 @@ ex<-readxl::read_excel('data/uk/uk_export.xlsx') %>% clean_names() %>%
 	group_by(species, indicator) %>% 
 	summarise(value = mean(value))
 
+ex$available<-nutS$tot[match(ex$species, nutS$species)]
+ex$prop_export<-ex$value / ex$available * 100
 
 ## order according to nut GHG
 ex$species<-factor(ex$species, levels=levels(fct_reorder(nut$species, nut$mid)[!duplicated(fct_reorder(nut$species, nut$mid))]))
@@ -33,8 +51,8 @@ ex$species<-factor(ex$species, levels=levels(fct_reorder(nut$species, nut$mid)[!
 ## 3. production 
 gg<-ggplot(ex, aes(value, species)) +
       geom_bar(stat = 'identity', fill='grey50') +
-      labs(x = expression(tonnes~yr^{'-1'}), y = '', parse=TRUE) +
-      scale_x_continuous(labels=scales::comma, expand=c(0, 0.06)) +
+      labs(x = expression(exported~tonnes~yr^{'-1'}), y = '', parse=TRUE) +
+      scale_x_continuous(labels=scales::comma, expand=c(0, 0.06), breaks=c(0, 25000, 50000, 75000, 100000,125000)) +
       th+ 
       theme(plot.margin=unit(c(0.1, 0.5, 0.1, 0.5), 'cm'), 
             axis.ticks = element_blank(), 
@@ -43,4 +61,21 @@ gg<-ggplot(ex, aes(value, species)) +
             # axis.text.y = element_blank(), 
             axis.title.y = element_blank(), strip.text.y = element_blank())
 
-gg
+
+gg1<-ggplot(ex, aes(prop_export, species)) +
+      geom_bar(stat = 'identity', fill='grey50') +
+      labs(x = 'proportion of available seafood exported, %', y = '', parse=TRUE) +
+      scale_x_continuous(labels=scales::comma, expand=c(0, 0.06)) +
+      th+ 
+      theme(plot.margin=unit(c(0.1, 0.5, 0.1, 0.5), 'cm'), 
+            axis.ticks = element_blank(), 
+            legend.position = c(0.85,0.5), 
+            panel.grid.major.x = element_line(size=0.2, colour ='grey'),
+            axis.text.y = element_blank(), 
+            axis.title.y = element_blank(), strip.text.y = element_blank())
+
+
+
+pdf(file = 'fig/final/FigureSX_UK_export.pdf', height=5, width=10)
+print(plot_grid(gg, gg1, nrow = 1, labels=c('A', 'B'), align='h', rel_widths=c(1, 0.7)))
+dev.off()
